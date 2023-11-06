6b42cc5ec8e086bd8ba363943132060390dcd778
Althaf S <althaf.sabsara@gmail.com>
2023-11-06 17:54:27 +0530

    Editing the commented lines
    Forgot to uncomment file while committing

--- gameoflife.py
+++ gameoflife.py
@@ -1,8 +1,8 @@
-#import numpy as np
+import numpy as np
 
-#def show_grid_in_terminal(shape_row,shape_column):
-#  my_array = np.full((shape_row,shape_column),"*", dtype = str)
-#  return str(my_array).replace(' [', '').replace("[", "").replace(']', '').replace("'", "")
+def show_grid_in_terminal(shape_row,shape_column):
+  my_array = np.full((shape_row,shape_column),"*", dtype = str)
+  return str(my_array).replace(' [', '').replace("[", "").replace(']', '').replace("'", "")
   
   
 
