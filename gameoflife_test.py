bfdc65e615c661c7923d8a931b47d1bd59295803
Althaf S <althaf.sabsara@gmail.com>
2023-11-08 10:08:42 +0530

    RED : Happens due to no function in main file

--- gameoflife_test.py
+++ gameoflife_test.py
@@ -1,15 +1,21 @@
 import gameoflife
 
 def test_none_alive():
-  alive = [6,6]
+  alive = [[]]
   size = [3,4]
   my_array = gameoflife.display_grid(size,alive)
   assert my_array == "* * * * * * * * * * * *"
   
 def test_one_alive():
-  alive = [2,1]
+  alive = [[2,1]]
   size = [3,4]
   my_array = gameoflife.display_grid(size,alive)
   assert my_array == "* * * * * * * * * A * *"
+  
+def test_more_than_one_alive():
+  alive = [[2,1],[1,1],[0,2]]
+  size = [3,4]
+  my_array = gameoflife.display_grid(size,alive)
+  assert my_array == "* * A * * A * * * A * *"
   
 
