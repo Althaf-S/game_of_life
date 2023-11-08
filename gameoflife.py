8e134236ba8c5c8758a1324c60d2312976747a8b
Althaf S <althaf.sabsara@gmail.com>
2023-11-08 10:09:46 +0530

    GREEN : Test runs successfully after adding proper functions
    function to input multiple alive positions

--- gameoflife.py
+++ gameoflife.py
@@ -1,13 +1,17 @@
 def display_grid(size,alive):
-  alive_row,alive_column = alive
+  #alive_row,alive_column = alive
   #print(alive_row,alive_column) 
   rows , columns = size
   positions = []
   for i in range(rows):
     for j in range(columns):
       positions.append([i,j])
+      #print(positions)
   my_grid = [["*" for j in range (columns)]  for i in range (rows)]
-  if alive in positions:
-     my_grid[alive_row][alive_column] = "A"
+  for i in alive:
+    if i in positions:
+      #print(i)
+      alive_row,alive_column = i 
+      my_grid[alive_row][alive_column] = "A"
   return str(my_grid).replace("[","").replace("]","").replace("'","").replace(",","")
 #print(display_grid())
