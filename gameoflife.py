90f8cb57ac9ffc44fadc06bd0765a5e5fe89e81d
Althaf S <althaf.sabsara@gmail.com>
2023-11-09 22:24:01 +0530

    GREEN : Function to test the updated grid is added

--- gameoflife.py
+++ gameoflife.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 def initialize_grid(rows, cols):
     return [[random.choice([0, 1]) for _ in range(cols)] for _ in range(rows)]
@@ -17,9 +18,19 @@ def count_neighbors(grid, x, y):
         if 0 <= i < rows and 0 <= j < cols:
             count += grid[i][j]
     return count
-
-
-
+def update_grid(grid):
+    new_grid = copy.deepcopy(grid)
+    rows, cols = len(grid), len(grid[0])
+    for i in range(rows):
+        for j in range(cols):
+            neighbors = count_neighbors(grid, i, j)
+            if grid[i][j]:
+                if neighbors < 2 or neighbors > 3:
+                    new_grid[i][j] = 0
+            else:
+                if neighbors == 3:
+                    new_grid[i][j] = 1
+    return new_grid
 
 
 
