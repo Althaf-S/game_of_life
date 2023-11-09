65feb857e5b8338925e5447e661ff971d3c53a86
Althaf S <althaf.sabsara@gmail.com>
2023-11-09 22:15:46 +0530

    GREEN : Function to test count of alive is added

--- gameoflife.py
+++ gameoflife.py
@@ -3,7 +3,20 @@ import random
 def initialize_grid(rows, cols):
     return [[random.choice([0, 1]) for _ in range(cols)] for _ in range(rows)]
 
-
+def print_grid(grid):
+    for row in grid:
+        print(' '.join(['*' if cell else ' ' for cell in row]))
+    print('\n')
+def count_neighbors(grid, x, y):
+    neighbors = [(x-1, y-1), (x-1, y), (x-1, y+1),
+                 (x, y-1),             (x, y+1),
+                 (x+1, y-1), (x+1, y), (x+1, y+1)]
+    rows, cols = len(grid), len(grid[0])
+    count = 0
+    for i, j in neighbors:
+        if 0 <= i < rows and 0 <= j < cols:
+            count += grid[i][j]
+    return count
 
 
 
